380015ba0031a33046f1db1f8bae15e53c1792bf
Sixplus <sixplus@sjtu.edu.cn>
2018-11-27 19:36:17 +0800

    First Commit

---
+++ trial/trial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  trial
+//
+//  Created by 刘佳 on 2018/11/27.
+//  Copyright © 2018 刘佳. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "learn_Git trial \n";
+    return 0;
+}
