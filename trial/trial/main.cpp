39fb5cb20aeb2663ff9f13a28aa2d296baf2137f
Sixplus <sixplus@sjtu.edu.cn>
2018-11-30 22:47:13 +0800

    bugFix

--- trial/trial/main.cpp
+++ trial/trial/main.cpp
@@ -5,9 +5,13 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    vector<string> trial;
+    trial.push_back("git");
     std::cout << "learn_Git trial \n";
     return 0;
 }
