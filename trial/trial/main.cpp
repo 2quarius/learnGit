18151cb280e049ecc0fb90b9b4defed986893353
Sixplus <sixplus@sjtu.edu.cn>
2018-11-30 22:42:16 +0800

    Third Trial

--- trial/trial/main.cpp
+++ trial/trial/main.cpp
@@ -5,9 +5,14 @@
 //
 
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
+    vector<string> trial;
+    trial.push_back("git");
+    trial.push_back("master");
     std::cout << "learn_Git trial \n";
+    cout << trial[0] <<endl;
     return 0;
 }
